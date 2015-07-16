4f74bd9144175398d8f093bb8310844dbd50c547
Eileen McNaughton <eileen@fuzion.co.nz>
2015-07-16 17:29:06 +1200

    found later version of this one :-)

--- CRM/Customsearches/Form/Search/agSpam.php
+++ CRM/Customsearches/Form/Search/agSpam.php
@@ -7,6 +7,9 @@
  * @author Andrew McNaughton
  */
 class CRM_Customsearches_Form_Search_agSpam extends CRM_Contact_Form_Search_Custom_Base implements CRM_Contact_Form_Search_Interface {
+
+  protected $_formValues;
+
   function __construct(&$formValues) {
     $this->_formValues = $formValues;
 
@@ -43,7 +46,7 @@ class CRM_Customsearches_Form_Search_agSpam extends CRM_Contact_Form_Search_Cust
       '7' => ts('Non-Numeric Postcode (and address not known to be overseas)'),
       '8' => ts('Long or short Postcode'),
       '9' => ts('Long or short Phone Number'),
-      '10' =>ts('Unexpected Punctuation in Name'),
+      '10' => ts('Unexpected Punctuation in Name'),
 
     );
     $form->addRadio('spam_options',
@@ -55,13 +58,13 @@ class CRM_Customsearches_Form_Search_agSpam extends CRM_Contact_Form_Search_Cust
     // Text box for phone number length to test
     $form->add('text',
       'min_length',
-      ts('Minimum Number of characters to test against phone number or postcode')
+      ts('Shortest length of postcodes (default: 4) or phone numbers (default: 10)')
     );
 
     // Text box for phone number length to test
     $form->add('text',
       'max_length',
-      ts('Maximum Number of characters to test against phone number or postcode')
+      ts('Longest length of postcodes (default: 4) or phone numbers (default: 14)')
     );
 
     // Date for records added since
@@ -79,41 +82,26 @@ class CRM_Customsearches_Form_Search_agSpam extends CRM_Contact_Form_Search_Cust
       ts("Check to not display contacts whose first and last name are both blank")
     );
 
+
     /**
      * If you are using the sample template, this array tells the template fields to render
      * for the search form.
      */
     $form->assign('elements', array(
       // 'spam_options', 'phone_length', 'postcode_length', 'start_date',
-      'spam_options', 'min_contact_id', 'blank_names', 'min_length', 'max_length',
+      'spam_options',
+      'min_contact_id',
+      'blank_names',
+      'min_length',
+      'max_length',
     ));
   }
 
   /**
-   * Get a list of summary data points
-   *
-   * @return mixed; NULL or array with keys:
-   *  - summary: string
-   *  - total: numeric
+   * Define the smarty template used to layout the search form and results listings.
    */
-  function summary() {
-    return NULL;
-  }
-
-  /**
-   * Get a list of displayable columns
-   *
-   * @return array, keys are printable column headers and values are SQL column names
-   */
-  function &columns() {
-    // return by reference
-    $columns = array(
-      ts('Contact Id') => 'contact_id',
-      ts('Contact Type') => 'contact_type',
-      ts('Name') => 'sort_name',
-      ts('State') => 'state_province',
-    );
-    return $columns;
+  function templateFile() {
+    return 'CRM/Contact/Form/Search/Custom.tpl';
   }
 
   /**
@@ -134,17 +122,18 @@ class CRM_Customsearches_Form_Search_agSpam extends CRM_Contact_Form_Search_Cust
     }
     else {
       $select = '
-        contact_a.id as contact_id,
-        contact_a.first_name  as first_name,
-        contact_a.last_name   as last_name,
-        email.email         as email,
-        phone.phone         as phone,
-        address.postal_code as postcode';
+                contact_a.id as contact_id,
+                contact_a.first_name  as first_name,
+                contact_a.last_name   as last_name,
+                email.email         as email,
+                phone.phone         as phone,
+                address.postal_code as postcode';
     }
 
     $from = $this->from();
 
     $where = $this->where($includeContactIDs);
+
 
     $sql = "
       SELECT $select
@@ -158,51 +147,37 @@ class CRM_Customsearches_Form_Search_agSpam extends CRM_Contact_Form_Search_Cust
       $sql .= " LIMIT $offset, $rowcount ";
     }
     return $sql;
-
   }
 
-  /**
-   * Construct a SQL SELECT clause
-   *
-   * @return string, sql fragment with SELECT arguments
-   */
-  function select() {
-    return "
-      contact_a.id           as contact_id  ,
-      contact_a.contact_type as contact_type,
-      contact_a.sort_name    as sort_name,
-      state_province.name    as state_province
-    ";
-  }
-
-  /**
-   * Construct a SQL FROM clause
-   *
-   * @return string, sql fragment with FROM and JOIN clauses
-   */
+  // from
   function from() {
+
+    $this->buildACLClause('contact_a');
+
     $sql = "civicrm_contact AS contact_a
       LEFT JOIN civicrm_email email
         ON contact_a.id = email.contact_id and email.is_primary=1
       LEFT JOIN civicrm_address address
         ON contact_a.id = address.contact_id AND address.is_primary=1
       LEFT JOIN civicrm_phone phone
-        ON contact_a.id = phone.contact_id AND phone.is_primary=1";
+        ON contact_a.id = phone.contact_id AND phone.is_primary=1
+        {$this->_aclFrom}";
 
 
     return $sql;
   }
 
   /**
-   * Construct a SQL WHERE clause
+   * WHERE clause is an array built from any required JOINS plus conditional filters based on search criteria field values
    *
-   * @param bool $includeContactIDs
-   * @return string, sql fragment with conditional expressions
    */
   function where($includeContactIDs = FALSE) {
     $clauses = array();
 
-    $clauses []= '(not contact_a.is_deleted)';
+    $clauses [] = '(not contact_a.is_deleted)';
+    if ($this->_aclWhere) {
+      $clauses[] = "{$this->_aclWhere}";
+    }
 
     $min_length = $this->_formValues['min_length'] + 0;
     $min_length < 10000 ? $min_length : 0;
@@ -225,43 +200,43 @@ END_SQL;
     switch ($spam_option) {
       case 1:
         // First name equals last name
-        $clauses[]   = "(contact_a.first_name = contact_a.last_name)";
+        $clauses[] = "(contact_a.first_name = contact_a.last_name)";
         break;
       case 2:
         // Names contain numbers
-        $clauses[]   = "(contact_a.first_name rlike '[0-9]')";
+        $clauses[] = "(contact_a.first_name rlike '[0-9]')";
         break;
       case 3:
         // Questionable email addresses
-        $clauses[]   = "(contact_a.first_name = contact_a.last_name)";
+        $clauses[] = "(contact_a.first_name = contact_a.last_name)";
         break;
       case 4:
         // Long postcodes
-        $clauses[]   = "(contact_a.first_name = contact_a.last_name)";
+        $clauses[] = "(contact_a.first_name = contact_a.last_name)";
         break;
       case 5:
         // Questionable phone numbers by length
-        $clauses[]   = "(contact_a.first_name = contact_a.last_name)";
+        $clauses[] = "(contact_a.first_name = contact_a.last_name)";
         break;
       case 6:
         // MiXEd CasE lASt NaME
-        $clauses[]   = "(contact_a.last_name rlike BINARY '[a-z][A-Z]+[a-z]+[A-Z]')";
+        $clauses[] = "(contact_a.last_name rlike BINARY '[a-z][A-Z]+[a-z]+[A-Z]')";
         break;
       case 7:
         // Non-Numeric Postcode
-        $clauses[]   = "((address.country_id = 1013 OR address.country_id IS NULL) AND (address.postal_code rlike '[^0-9 ]'))";
+        $clauses[] = "((address.country_id = 1013 OR address.country_id IS NULL) AND (address.postal_code rlike '[^0-9 ]'))";
         break;
       case 8:
         // Long or short Postcode
-        $clauses[]   = "(length(address.postal_code) < $min_length OR length(address.postal_code) > $max_length )";
+        $clauses[] = "(length(address.postal_code) < $min_length OR length(address.postal_code) > $max_length )";
         break;
       case 9:
         // Long or short Phone Number
-        $clauses[]   = "(length(phone) < $min_length OR length(phone) > $max_length )";
+        $clauses[] = "(length(phone) < $min_length OR length(phone) > $max_length )";
         break;
       case 10:
         // Unexpected Punctuation in Name
-        $clauses[]   = <<<END_SQL
+        $clauses[] = <<<END_SQL
             ((contact_a.first_name rlike $unexpected_name_chars)
           OR (contact_a.last_name rlike $unexpected_name_chars)
             )
@@ -270,14 +245,15 @@ END_SQL;
     }
 
     $min_contact_id = $this->_formValues['min_contact_id'];
-    if($min_contact_id) {
-      $clauses []= "(contact_a.id > $min_contact_id)";
+    if ($min_contact_id) {
+      $clauses [] = "(contact_a.id > $min_contact_id)";
     }
 
     $blank_names = $this->_formValues['blank_names'];
-    if($blank_names) {
-      $clauses []= "(contact_a.first_name AND contact_a.last_name)";
+    if ($blank_names) {
+      $clauses [] = "(contact_a.first_name AND contact_a.last_name)";
     }
+
 
     if ($includeContactIDs) {
       $contactIDs = array();
@@ -298,22 +274,32 @@ END_SQL;
     return implode(' AND ', $clauses);
   }
 
-  /**
-   * Determine the Smarty template for the search screen
-   *
-   * @return string, template path (findable through Smarty template path)
+  /*
+   * Functions below generally don't need to be modified
    */
-  function templateFile() {
-    return 'CRM/Contact/Form/Search/Custom.tpl';
+  function count() {
+    $sql = $this->all();
+
+    $dao = CRM_Core_DAO::executeQuery($sql,
+      CRM_Core_DAO::$_nullArray
+    );
+    return $dao->N;
   }
 
-  /**
-   * Modify the content of each row
-   *
-   * @param array $row modifiable SQL result row
-   * @return void
-   */
-  function alterRow(&$row) {
-    $row['sort_name'] .= ' ( altered )';
+  function contactIDs($offset = 0, $rowcount = 0, $sort = NULL) {
+    return $this->all($offset, $rowcount, $sort, FALSE, TRUE);
   }
+
+  function &columns() {
+    return $this->_columns;
+  }
+
+  function summary() {
+    return NULL;
+  }
+
+  function buildACLClause($tableAlias = 'contact') {
+    list($this->_aclFrom, $this->_aclWhere) = CRM_Contact_BAO_Contact_Permission::cacheClause($tableAlias);
+  }
+
 }
